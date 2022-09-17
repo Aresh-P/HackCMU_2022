5fbecefaa7ee8bccd2dacb60836a0a3f70c70673
Luca Borletti <lgborletti@gmail.com>
2022-09-16 23:16:09 -0400

    added interactjs

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,5 +1,27 @@
+function getOffset(el) {
+    const rect = el.getBoundingClientRect();
+    return {
+      left: rect.left + window.scrollX,
+      top: rect.top + window.scrollY,
+      width: el.offsetWidth,
+      height: el.offsetHeight
+    };
+  }
+
 window.onload = function() {
-    let test = new Draggable.Draggable(document.getElementById('givens-playground'), {
-        
+    let test = new Draggable.Droppable(document.getElementById('givens-playground'), {
+        draggable: 'div'
+        // dropzone: 
     });
+
+    let box = document.getElementById('box');
+    
+    // console.log(box.getAttribute("left"));
+    // console.log(box.getAttribute(""));
+    console.log(getOffset(box).left);
+    console.log(getOffset(box).top);
+    console.log(getOffset(box).width);
+    console.log(getOffset(box).height);
+
 }
+
