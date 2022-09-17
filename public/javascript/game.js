78599574fc2b3bbb242125d73b2782969bdc0028
ivanbur <ivan@burgert.com>
2022-09-17 07:09:17 -0400

    fixed bug

--- public/javascript/game.js
+++ public/javascript/game.js
@@ -30,71 +30,82 @@ let exampleenvironments = [
     }
 ];
 
+
+
+
 function main() {
     currentLevel = new Level(exampleenvironments);
     $('body').append(currentLevel.element);
 }
 
-
-function dragMoveListener (event) {
-    var target = event.target
-    // keep the dragged position in the data-x/data-y attributes
-    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-
-    // translate the element
-    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-
-    // update the posiion attributes
-    target.setAttribute('data-x', x)
-    target.setAttribute('data-y', y)
+function dragging() {
+    function dragMoveListener (event) {
+        var target = event.target
+        // keep the dragged position in the data-x/data-y attributes
+        var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+        var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+    
+        // translate the element
+        target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+    
+        // update the posiion attributes
+        target.setAttribute('data-x', x)
+        target.setAttribute('data-y', y)
+    }
+    
+    interact('.draggable').draggable({
+        inertia: true,
+        modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: 'parent',
+                endOnly: true
+            })
+        ],
+        autoScroll: false,
+        listeners: {
+            start: function (event) {
+                let rect = event.rect;
+                console.log(event.rect);
+                // $(event.target).attr('data-x-revert', $(event.target).attr('data-x'));
+                // $(event.target).attr('data-y-revert', $(event.target).attr('data-y'));
+                $(event.target.parentElement).prepend($(event.target.outerHTML).css({position: 'absolute', top: rect.top, left: rect.left}).attr('id', 'kill').removeClass('draggable'));
+                console.log($('#kill').css('top'));
+            },
+            move: dragMoveListener,
+            end: function (event) {
+                // event.target.style.transform = 'translate(' + $(event.target).attr('data-x-revert') + 'px, ' + $(event.target).attr('data-y-revert') + 'px)'
+                // $(event.target).attr('data-x', $(event.target).attr('data-x-revert'));
+                // $(event.target).attr('data-y', $(event.target).attr('data-y-revert'));
+                $('#kill').remove();
+            }
+        }
+    });
 }
 
-interact('.draggable').draggable({
-    inertia: true,
-    modifiers: [
-        interact.modifiers.restrictRect({
-            restriction: 'parent',
-            endOnly: true
-        })
-    ],
-    autoScroll: false,
-    listeners: {
-        start: function (event) {
-            let rect = event.rect;
-            console.log(event.rect);
-            // $(event.target).attr('data-x-revert', $(event.target).attr('data-x'));
-            // $(event.target).attr('data-y-revert', $(event.target).attr('data-y'));
-            $(event.target.parentElement).prepend($(event.target.outerHTML).css({position: 'absolute', top: rect.top, left: rect.left}).attr('id', 'kill'));
-        },
-        move: dragMoveListener,
-        end: function (event) {
-            // event.target.style.transform = 'translate(' + $(event.target).attr('data-x-revert') + 'px, ' + $(event.target).attr('data-y-revert') + 'px)'
-            // $(event.target).attr('data-x', $(event.target).attr('data-x-revert'));
-            // $(event.target).attr('data-y', $(event.target).attr('data-y-revert'));
-            $('#kill').remove();
+function addClasses() {
+    $('.givens-playground > *').each(function() {
+        console.log('test');
+        let classString = $(this).attr('class');
+        if (classString.substr(0, 3) === 'imp') {
+            $(this).children('.connector-wrapper').children('.connector-left').addClass('inactive-constructor');
+    
+    
+            let acceptstr = '.' + classString.substr(classString.lastIndexOf('left-') + 5);
+            interact($(this).children('.connector-wrapper').children('.connector-left')[0]).dropzone({
+                accept: acceptstr,
+                overlap: 0.75,
+    
+                ondrop: function (event) {
+                    // $(this).children('.connector-wrapper').children('.connector-left').removeClass('inactive-constructor');
+    
+                    event.target.classList.remove('inactive-constructor');
+                }
+            });
         }
-    }
-});
+    });
+}
 
-$('.givens-playground > *').each(function() {
-    let classString = $(this).attr('class');
-    if (classString.substr(0, 3) === 'imp') {
-        $(this).children('.connector-wrapper').children('.connector-left').addClass('inactive-constructor');
-
-
-        let acceptstr = '.' + classString.substr(classString.lastIndexOf('left-') + 5);
-        interact($(this).children('.connector-wrapper').children('.connector-left')[0]).dropzone({
-            accept: acceptstr,
-            overlap: 0.75,
-
-            ondrop: function (event) {
-                // $(this).children('.connector-wrapper').children('.connector-left').removeClass('inactive-constructor');
-
-                event.target.classList.remove('inactive-constructor');
-            }
-        });
-    }
-});
 
 main();
+dragging();
+addClasses();
