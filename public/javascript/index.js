484e1a5f3728d4600972ca6139520b1b89a6dc75
Luca Borletti <lgborletti@gmail.com>
2022-09-17 02:18:51 -0400

    uncommented html replacement in javascript

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -87,22 +87,22 @@ window.onload = function() {
     });
 }
 
-let givens = document.getElementById('givens-playground');
-    let expr =
-	new Machine(
-	    new Machine (
-		new Atom('P'),
-		'and',
-		new Atom('Q'),
-	    ),
-	    'imp',
-	    new Machine(
-		new Atom('R'),
-		'or',
-		new Atom('S'),
-	    )
-);
-givens.innerHTML = expr.element.outerHTML;
+// let givens = document.getElementById('givens-playground');
+//     let expr =
+// 	new Machine(
+// 	    new Machine (
+// 		new Atom('P'),
+// 		'and',
+// 		new Atom('Q'),
+// 	    ),
+// 	    'imp',
+// 	    new Machine(
+// 		new Atom('R'),
+// 		'or',
+// 		new Atom('S'),
+// 	    )
+// );
+// givens.innerHTML = expr.element.outerHTML;
 
 // function getOffset(el) {
 //     const rect = el.getBoundingClientRect();
